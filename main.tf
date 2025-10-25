9b850250ac00946fb9522f124c0cfcdfef855819
badrinadhb1012 <badrinadh1012@gmail.com>
2025-10-25 10:45:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,44 +1,47 @@
 resource "aws_launch_template" "web_server_as" {
-    name = "myproject"
-    image_id           = "ami-00af95fa354fdb788"
-    vpc_security_group_ids = [aws_security_group.web_server.id]
-    instance_type = "t3.micro"
-    key_name = "karthikmumbai1"
+  name = "myproject"
+  image_id = "ami-00af95fa354fdb788"
+  instance_type = "t3.micro"
+  key_name = "karthikmumbai1"
+
+  vpc_security_group_ids = [aws_security_group.web_server.id]
+
+  tag_specifications {
+    resource_type = "instance"
     tags = {
-        Name = "DevOps"
+      Name = "DevOps"
     }
-    
+  }
 }
-   
 
+resource "aws_elb" "web_server_lb" {
+  name               = "web-server-lb"
+  security_groups    = [aws_security_group.web_server.id]
+  subnets            = ["subnet-0c0db740f69a89471", "subnet-0de6a940154438e27"]
 
-  resource "aws_elb" "web_server_lb"{
-     name = "web-server-lb"
-     security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-0c0db740f69a89471, "subnet-0de6a940154438e27"]
-     listener {
-      instance_port     = 8000
-      instance_protocol = "http"
-      lb_port           = 80
-      lb_protocol       = "http"
-    }
-    tags = {
-      Name = "terraform-elb"
-    }
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
   }
+
+  tags = {
+    Name = "terraform-elb"
+  }
+}
+
 resource "aws_autoscaling_group" "web_server_asg" {
-    name                 = "web-server-asg"
-    min_size             = 1
-    max_size             = 3
-    desired_capacity     = 2
-    health_check_type    = "EC2"
-    load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["ap-south-1c", "ap-south-1a"] 
-    launch_template {
-        id      = aws_launch_template.web_server_as.id
-        version = "$Latest"
-      }
-    
-    
-  }
+  name                 = "web-server-asg"
+  min_size             = 1
+  max_size             = 3
+  desired_capacity     = 2
+  health_check_type    = "EC2"
+  load_balancers       = [aws_elb.web_server_lb.name]
+  availability_zones   = ["ap-south-1c", "ap-south-1a"]
 
+  launch_template {
+    id      = aws_launch_template.web_server_as.id
+    version = "$Latest"
+  }
+}
